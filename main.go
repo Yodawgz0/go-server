5b60581d2829982b30caac7fc2d16562b3ca67d8
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2024-02-07 16:08:47 -0500

    refactor of code

--- main.go
+++ main.go
@@ -5,7 +5,22 @@ import (
 	"net/http"
 )
 
+// Define a type that implements the http.Handler interface
+type tableHandler struct{}
+
+// Implement the ServeHTTP method for the tableHandler type
+func (h *tableHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Check the request method and handle accordingly
+	switch r.Method {
+	case http.MethodGet:
+		handleReadGetRequest(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
 func main() {
+	// Use the http.HandleFunc function for the root path
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
@@ -13,18 +28,12 @@ func main() {
 		case http.MethodPost:
 			handlePostRequest(w, r)
 		default:
-			http.Error(w, "Methodd not allowed", http.StatusMethodNotAllowed)
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		}
 	})
+	th := &tableHandler{}
+	http.Handle("/readTables", th)
 
-	http.HandleFunc("/readTables", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			handleReadGetRequest(w, r)
-		default:
-			http.Error(w, "Methodd not allowed", http.StatusMethodNotAllowed)
-		}
-	})
 	// Start the HTTP server
 	log.Println("Server is running on :8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
