40d08263a160216b33db8016e1a6e2efb748f8ea
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2024-02-18 17:09:47 -0500

    adding middleware

---
+++ middlewares.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"net/http"
+)
+
+func middleware(handler func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Set common headers
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		// Handle preflight OPTIONS request
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		switch r.URL.Path {
+		case "/userLogin", "/userLogout":
+			if r.Method != http.MethodPost {
+				http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+				return
+			}
+		default:
+			if r.Method != http.MethodGet && r.Method != http.MethodPost {
+				http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+				return
+			}
+		}
+
+		handler(w, r)
+	}
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+	isValid, err := verifyTokenHandler(w, r)
+	if err != nil || !isValid {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	switch r.Method {
+	case http.MethodGet:
+		handleGetRequest(w, r)
+	case http.MethodPost:
+		handleYearFilter(w, r)
+	}
+}
