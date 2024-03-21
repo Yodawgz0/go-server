9232d285317c7f259089429080edc2419cc12a0b
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2024-03-20 23:55:52 -0400

    updating data type

--- services.go
+++ services.go
@@ -15,11 +15,17 @@ type Year struct {
 }
 
 type CensusData struct {
-	ID         int    `json:"id"`
-	Geo        string `json:"geo"`
-	Name       string `json:"name"`
-	Time       int    `json:"time"`
-	Population int    `json:"population"`
+	Key                  string            `json:"key"`
+	ArrayContains        map[string]string `json:"array_contains"`
+	ArraySize            *int              `json:"array_size,omitempty"`
+	DocJSON              map[string]string `json:"doc_json"`
+	ExistKeys            []string          `json:"exist_keys"`
+	QueryBoolValues      *bool             `json:"query_bool_values,omitempty"`
+	QueryDblValues       *float64          `json:"query_dbl_values,omitempty"`
+	QueryNullValues      *bool             `json:"query_null_values,omitempty"`
+	QueryTextValues      map[string]string `json:"query_text_values"`
+	QueryTimestampValues *string           `json:"query_timestamp_values,omitempty"`
+	TxID                 string            `json:"tx_id"`
 }
 
 func handleGetRequest(w http.ResponseWriter, r *http.Request) {
