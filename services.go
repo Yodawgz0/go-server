bba0bde4e18d9c4539e60ccd10da5a634a376e5c
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2024-03-23 23:43:24 -0400

    working with new ds

--- services.go
+++ services.go
@@ -28,35 +28,64 @@ type CensusData struct {
 	TxID                 string            `json:"tx_id"`
 }
 
+//	cluster.Authenticator = gocql.PasswordAuthenticator{
+//		Username: "YWTfzyivjkJufvUzSRNKiNvZ",
+//		Password: "bMnajGxe6ZGj2nPycO6d4m8MS+4FMPtsQc831uJ02zJoHHSq0pGuMO_52kuZSA5rv8gY-.e8DxiSghOf60Zca2ME-JS.0--z_2imZL5tFrnGZ8LqZn+aO5ZCFjxJzhln",
+//	}
 func handleGetRequest(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Recieved GET reuqest from %s for %s", r.RemoteAddr, r.URL.Path)
 	cluster := gocql.NewCluster("localhost")
 	cluster.Port = 9042
-	// cluster.Authenticator = gocql.PasswordAuthenticator{
-	// 	Username: "ashley",
-	// 	Password: "bazzi",
-	// }
-	cluster.Keyspace = "datapop"
+	cluster.Keyspace = "census_data"
 	session, err := cluster.CreateSession()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer session.Close()
-	iter := session.Query("SELECT * FROM census WHERE time = 2100 ALLOW FILTERING").Iter()
-	var censusData CensusData
-	censusRecords := []CensusData{}
-	for iter.Scan(&censusData.ID, &censusData.Geo, &censusData.Name, &censusData.Population, &censusData.Time) {
-		censusRecords = append(censusRecords, censusData)
+
+	// Execute the query
+	query := "SELECT * FROM census_data WHERE query_text_values['time'] = '1841' LIMIT 5 ALLOW FILTERING"
+	iter := session.Query(query).Iter()
+
+	// Slice to hold the results
+	var results []CensusData
+
+	// Map to hold the row data
+	var row map[string]interface{}
+
+	// Iterate through the results
+	for iter.MapScan(row) {
+		// Convert the row to CensusData and append to results
+
+		if !iter.MapScan(row) {
+			break
+		}
+		data := CensusData{
+			Key:             row["key"].(string),
+			ArrayContains:   row["array_contains"].(map[string]string),
+			DocJSON:         row["doc_json"].(map[string]string),
+			ExistKeys:       row["exist_keys"].([]string),
+			QueryTextValues: row["query_text_values"].(map[string]string),
+			TxID:            row["tx_id"].(string),
+		}
+		results = append(results, data)
 	}
-	jsonData, err := json.Marshal(censusRecords)
+
+	// Check for errors in iteration
+	if err := iter.Close(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Marshal the results to JSON
+	jsonData, err := json.Marshal(results)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	_, _ = w.Write(jsonData)
-
+	w.Write(jsonData)
 }
 func handleYearFilter(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Received POST request from %s for %s", r.RemoteAddr, r.URL.Path)
