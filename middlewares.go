4ed6c72182cb27c048e5a91850c00a0e6da65db0
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2024-03-27 19:03:25 -0400

    code refactor adding year filter

--- middlewares.go
+++ middlewares.go
@@ -35,15 +35,17 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	isValid, err := verifyTokenHandler(w, r)
-	if err != nil || !isValid {
-		http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		return
-	}
-	switch r.Method {
-	case http.MethodGet:
-		handleGetRequest(w, r)
-	case http.MethodPost:
-		handleYearFilter(w, r)
+
+	switch r.URL.Path {
+	case "/census_data":
+		if r.Method == http.MethodGet {
+			yearFilter := r.URL.Query().Get("year")
+			handleYearFilterRequest(w, r, yearFilter)
+		}
+	default:
+		if r.Method != http.MethodGet && r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
 	}
 }
